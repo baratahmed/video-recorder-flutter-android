527fd65f147402ef7ae82fc4b96bcd6f5b823240
baratahmed <baratahmed28@gmail.com>
2023-10-13 23:01:22 +0600

    First Commit

---
+++ android/app/src/main/kotlin/com/example/cd_video_recorder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cd_video_recorder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/info.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+// import 'package:cd_video_recorder/screens/home_screen.dart';
+// import 'package:cd_video_recorder/screens/camera_screen.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   cameras = await availableCameras();
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key key}) : super(key: key);
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Video Recorder',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home:  HomeScreen(),
+//     );
+//   }
+// }
+//
